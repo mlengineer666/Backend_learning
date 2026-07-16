dd3b03e65930a62a98f0e6eba0811c4596732448
S-A-N-D-I-P-A-N <149672581+mlengineer666@users.noreply.github.com>
2026-07-16 11:37:11 +0000

    changes_made

--- jsLearning/Basics.js
+++ jsLearning/Basics.js
@@ -47,7 +47,7 @@ console.log(age); // not error bcz defined with var and it is function scope
 
 /* Interview pov imp */
 // Hoisting
-console.log(greeting);   // undefined special data types
+// console.log(greeting);   // undefined special data types
 var greeting = 'Hello';  // applicable only in var ,not in (const and let)
 
 // use const and let , var is old
@@ -55,8 +55,105 @@ var greeting = 'Hello';  // applicable only in var ,not in (const and let)
 const num = 45;
 console.log(num);
 
+/* JS_7 */
+
+// e.g of dynamic typing
+
+let my_age = 15;
+my_age = 'Sandipan'
+
+console.log(my_age)
+
+/* Primitive datatypes : single value can be stored . e.g- True/False, String etc*/
+// 1. Number : decimal numbers, +ve number -ve number
+let Age = 25;
+let temp = -25;
+let price = 50.50;
+
+// 2. String: textual data
+let name = 'Sandipan';
+
+// 3. Boolean : True/False
+let isLearningJS = true;
+let isFinish = false;
+let hasPermission = false;
 
 
+// 4. undefined ?? Those variables did not assign any value 
+
+let myName
+console.log(myName); // Showing undefined
+
+// 5. BigInt: max safe integer 
+
+console.log(Number.MAX_SAFE_INTEGER) // [(2**53) -1]
+console.log(Number.MIN_SAFE_INTEGER)  // -2**53
+// using BigInt we can store more than max_safe value
+
+
+/* 2. Structural Datatypes */
+// 1. object 
+//   1.1 Functions : Non-data structure
+//   1.2 Arrays : collection of values (dynamic type like Python language)
+// we can store objects also.
+const number = [2,3,4,5,'JS','Python',{name:'sandipan',age:21,Gender:'Male'}]
+console.log(number[6].Gender)
+//   1.3 Maps
+//   1.4 Set
+//   1.5 Date
+
+//   3. Structural root
+        // 3.1 null
+
+/* Operators */
+// Assignment Operators
+let y =  {name:'sandip'}
+x = 10
+z = 20
+console.log(y.name, x ,z)
+
+
+
+// Conditional Operators(Ternary Operators)
+let userRole = 'Admin';
+
+select_role = userRole === 'Admin' ? 'You are an Admin' : 'You are not an Admin';
+
+console.log(select_role)
+
+/* NOTE: === is the strict equality and !== is strict inequality */
+
+// 4 == "4" → true (coercion)  
+
+// 4 === "4" → false (strict — no coercion) 
+
+/* Comparision Operator  */
+
+// ===     Strict Equal
+
+// !==     Strict Not Equal
+
+// ==      Loose Equal
+
+// !=      Loose Not Equal
+
+// >       Greater Than
+
+// <       Less Than
+
+// <=      Lesser Than or Equal
+
+// >=      Greater Than or Equal
+
+/* Coercion is the process of converting data */
+console.log(4 == '4') // true bcz of losse equal
+console.log(4 === '4') // false of strict equal
+
+console.log(3+10*3)
+console.log((3+2)*8)
+
+console.log(3 > 2 && 2 > 1)
+console.log(3 > 2 > 1)
 
 
 
